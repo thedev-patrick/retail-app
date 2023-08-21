0f581774727d74263e5f86d8827654fea7b04570
Patrick Oguamanam <kachioguamanam@gmail.com>
2023-08-21 17:19:10 +0100

    qrCode fixes

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { v4: uuidv4 } = require('uuid');
 const app = express();
+const QRCode = require('qrcode');
 const PORT = process.env.PORT || 3000;
 
 // Middleware
@@ -10,6 +11,28 @@ app.use(bodyParser.json());
 // Routes
 app.use('/customers', require('./routes/customerRoutes'));
 app.use('/transactions', require('./routes/transactionRoutes'));
+app.get('/generate-qrcode/:transactionId', async (req, res) => {
+    try {
+      const { transactionId } = req.params;
+  
+      // Fetch transaction details from dataStore or wherever you store it
+      const transactionDetails = dataStore.getTransaction(transactionId);
+      if (!transactionDetails) {
+        return res.status(404).json({ status: 'error', message: 'Transaction not found' });
+      }
+  
+      const qrCodeText = JSON.stringify(transactionDetails);
+  
+      // Generate QR code
+      const qrCodeUrl = await QRCode.toDataURL(qrCodeText);
+  
+      res.status(200).json({ qrCodeUrl });
+    } catch (error) {
+      console.error('Error generating QR code:', error);
+      res.status(500).json({ status: 'error', message: 'Internal server error' });
+    }
+  });
+  
 
 // Health Check
 app.get('/health', (req, res) => {
