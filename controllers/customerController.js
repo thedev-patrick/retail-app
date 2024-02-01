6db44338346272abed74b6efa47574de915bb3a5
Patrick Oguamanam <kachioguamanam@gmail.com>
2024-02-01 13:07:02 +0100

    shortened customer number

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,6 +1,5 @@
 const db = require("../db");
 const { faker } = require("@faker-js/faker");
-const { v4: uuidv4 } = require("uuid");
 const customers = {};
 
 // Assuming you have a database connection pool, replace it with your actual database library
@@ -20,7 +19,7 @@ exports.createCustomer = async (req, res) => {
   // Additional checks if needed, e.g., format, length, uniqueness, etc.
 
   // Generate customer_id, customer_name, and created_at
-  const customer_id = `PL${uuidv4()}`; // Generate a unique ID
+  const customer_id = `R${generateRandomNumber(100000, 999999)}`;
   const customer_name = faker.person.fullName();
   const created_at = new Date();
 
@@ -44,35 +43,30 @@ exports.createCustomer = async (req, res) => {
         .status(201)
         .json({ customer_id, customer_name, mobile_no, nin, created_at });
     } else {
-      return res
-        .status(400)
-        .json({
-          status: "error",
-          message: "Customer with the same mobile number or NIN already exists",
-        });
+      return res.status(400).json({
+        status: "error",
+        message: "Customer with the same mobile number or NIN already exists",
+      });
     }
-
   } catch (error) {
     console.error("Error inserting customer into the database:", error);
     res.status(500).json({ status: "error", message: "Internal server error" });
   }
 };
 
-exports.getCustomers = async(req, res) => {
-   try {
-     // Retrieve all customers from the database
-     const allCustomers = await db.query("SELECT * FROM customers");
+exports.getCustomers = async (req, res) => {
+  try {
+    // Retrieve all customers from the database
+    const allCustomers = await db.query("SELECT * FROM customers");
 
-     // Check if there are any customers
-     if (allCustomers[0].length > 0) {
-       res.status(200).json(allCustomers[0]);
-     } else {
-       res.status(404).json({ status: "error", message: "No customers found" });
-     }
-   } catch (error) {
-     console.error("Error retrieving customers from the database:", error);
-     res
-       .status(500)
-       .json({ status: "error", message: "Internal server error" });
-   }
-}
+    // Check if there are any customers
+    if (allCustomers[0].length > 0) {
+      res.status(200).json(allCustomers[0]);
+    } else {
+      res.status(404).json({ status: "error", message: "No customers found" });
+    }
+  } catch (error) {
+    console.error("Error retrieving customers from the database:", error);
+    res.status(500).json({ status: "error", message: "Internal server error" });
+  }
+};
